0d6d9559cd86c062de7fc32169035f680fbfab99
RoderCode <Rodersnow@gmail.com>
2023-02-08 14:52:14 +0100

    add ChatRoom Entity

--- src/main/java/com/example/phone_duck/entity/ChatRoom.java
+++ src/main/java/com/example/phone_duck/entity/ChatRoom.java
@@ -1,4 +1,23 @@
 package com.example.phone_duck.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "channels", uniqueConstraints = {
+        @UniqueConstraint(name = "chatRoom_name_unique", columnNames = "name")
+})
 public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String title;
 }
