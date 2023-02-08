aa8e87d8a970618ca962fe70cde4b80cabfe5313
RoderCode <Rodersnow@gmail.com>
2023-02-08 14:57:24 +0100

    add connection to repo from service

--- src/main/java/com/example/phone_duck/repo/ChatRoomRepo.java
+++ src/main/java/com/example/phone_duck/repo/ChatRoomRepo.java
@@ -1,7 +1,10 @@
 package com.example.phone_duck.repo;
 
 import com.example.phone_duck.entity.ChatRoom;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatRoomRepo extends JpaRepository<ChatRoom,Long> {
 }
--- src/main/java/com/example/phone_duck/service/ChatRoomService.java
+++ src/main/java/com/example/phone_duck/service/ChatRoomService.java
@@ -1,4 +1,15 @@
 package com.example.phone_duck.service;
 
+import com.example.phone_duck.entity.ChatRoom;
+import com.example.phone_duck.repo.ChatRoomRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChatRoomService {
+    @Autowired
+    private ChatRoomRepo chatRoomRepo;
+
+    
+
 }
