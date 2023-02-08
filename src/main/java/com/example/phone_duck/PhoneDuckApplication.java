da7069648008fba580409ba42580181ce3b1dfee
RoderCode <Rodersnow@gmail.com>
2023-02-08 13:42:13 +0100

    my first commit

---
+++ src/main/java/com/example/phone_duck/PhoneDuckApplication.java
@@ -0,0 +1,13 @@
+package com.example.phone_duck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneDuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneDuckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phone_duck/PhoneDuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phone_duck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneDuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
