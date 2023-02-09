4073d7489bb58839c4a606506ea2a31e43d4a4c6
RoderCode <Rodersnow@gmail.com>
2023-02-09 11:05:14 +0100

    add 404 status code when user try to delete something that doesn't exist

--- src/main/java/com/example/phone_duck/api/ChatRoomController.java
+++ src/main/java/com/example/phone_duck/api/ChatRoomController.java
@@ -3,7 +3,10 @@ package com.example.phone_duck.api;
 import com.example.phone_duck.entity.ChatRoom;
 import com.example.phone_duck.service.ChatRoomService;
 import lombok.Getter;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +24,7 @@ public class ChatRoomController {
         if (chatRoomService.readAll().isEmpty()){
             return ResponseEntity
                     .status(204)
-                    .header("x-information", "there are none ChatRoom active")
+                    .header("x-information", "there are no ChatRooms active")
                     .build();
         }
         return new ResponseEntity<>(chatRoomService.readAll(), HttpStatus.OK);
@@ -29,13 +32,27 @@ public class ChatRoomController {
 
     @PostMapping("create")
     private ResponseEntity<String> createChatRoom(@RequestBody ChatRoom chatRoom) {
-        chatRoomService.create(chatRoom);
+        try {
+            chatRoomService.create(chatRoom);
+        }catch (DataIntegrityViolationException e){
+            return ResponseEntity
+                    .status(422)
+                    .header("error-information","Please enter a different name, this name already exist")
+                    .build();
+        }
         return new ResponseEntity<>("Chat Room was created",HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}/delete")
     private ResponseEntity<String> deleteChatRoom(@PathVariable("id") Long id){
-        chatRoomService.delete(id);
+        try{
+            chatRoomService.delete(id);
+        }catch (EmptyResultDataAccessException e){
+           return ResponseEntity
+                   .status(404)
+                   .header("x-information","ChatRoom you were tried to delete does not exist")
+                   .build();
+        }
         return new ResponseEntity<>("Chat Room was deleted",HttpStatus.OK) ;
     }
 
