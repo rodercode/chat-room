cf68e23658f413b9babf347578f8b12a88fdbb0d
RoderCode <Rodersnow@gmail.com>
2023-02-09 17:47:55 +0100

    fix connection to socket chatroom

--- src/main/java/com/example/phone_duck/config/SocketConfig.java
+++ src/main/java/com/example/phone_duck/config/SocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.phone_duck.config;
 
+import com.example.phone_duck.websocket.ChatRoomSocketHandler;
 import com.example.phone_duck.websocket.MainChatRoomSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +13,12 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class SocketConfig implements WebSocketConfigurer {
 
     @Autowired
-    private MainChatRoomSocketHandler chatRoomSocketHandler;
-    private String name = "chatroom";
+    private MainChatRoomSocketHandler mainChatRoomSocketHandler;
+    @Autowired
+    private ChatRoomSocketHandler chatRoomSocketHandler;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatRoomSocketHandler,"/sub/channels");
-        registry.addHandler(new MainChatRoomSocketHandler(), "/sub/chat/"+ name);
+        registry.addHandler(mainChatRoomSocketHandler,"/sub/channels");
+        registry.addHandler(chatRoomSocketHandler,"/sub/chat");
     }
 }
--- src/main/java/com/example/phone_duck/websocket/ChatRoomSocketHandler.java
+++ src/main/java/com/example/phone_duck/websocket/ChatRoomSocketHandler.java
@@ -1,22 +1,28 @@
 package com.example.phone_duck.websocket;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Component
 public class ChatRoomSocketHandler extends TextWebSocketHandler {
 
     private List<WebSocketSession> webSocketSessions = new ArrayList<>();
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        broadcast(message.getPayload());
+    }
+
+    public void broadcast(String message) throws IOException {
         for (WebSocketSession webSocketSession : webSocketSessions) {
-            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+            webSocketSession.sendMessage(new TextMessage(message));
         }
     }
     @Override
