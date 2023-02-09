ff862e397fc3caa0bafe4663674c63f9040d0a71
RoderCode <Rodersnow@gmail.com>
2023-02-09 11:30:04 +0100

    add socketHandle for MainChat

--- src/main/java/com/example/phone_duck/websocket/MainChatRoomSocketHandler.java
+++ src/main/java/com/example/phone_duck/websocket/MainChatRoomSocketHandler.java
@@ -1,14 +1,37 @@
 package com.example.phone_duck.websocket;
 
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainChatRoomSocketHandler {
+public class MainChatRoomSocketHandler extends TextWebSocketHandler {
 
     private List<WebSocketSession> webSocketSessions = new ArrayList<>();
 
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : webSocketSessions) {
+            webSocketSession.sendMessage(new TextMessage("Message: " + message.getPayload()));
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+        webSocketSessions.add(session);
+        System.out.println("Create Session");
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        webSocketSessions.remove(session);
+        System.out.println("End Session");
+    }
 }
