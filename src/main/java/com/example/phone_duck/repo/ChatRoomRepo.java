eb0a7b5b24f3e1702767e2ccdb09b26d747e868c
RoderCode <Rodersnow@gmail.com>
2023-02-10 13:39:22 +0100

    add a crud operations that show all active chatrooms

--- src/main/java/com/example/phone_duck/repo/ChatRoomRepo.java
+++ src/main/java/com/example/phone_duck/repo/ChatRoomRepo.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRoomRepo extends JpaRepository<ChatRoom,Long> {
+
+    List<ChatRoom> findAllByIsOnlineTrue();
 }
