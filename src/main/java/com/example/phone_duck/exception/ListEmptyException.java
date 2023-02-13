8aeaaf77bbc599d81a9edb88f921e934585c02c1
RoderCode <Rodersnow@gmail.com>
2023-02-13 10:51:28 +0100

    add ListEmptyException

---
+++ src/main/java/com/example/phone_duck/exception/ListEmptyException.java
@@ -0,0 +1,12 @@
+package com.example.phone_duck.exception;
+
+public class ListEmptyException extends RuntimeException {
+
+    public ListEmptyException(String message) {
+        super(message);
+    }
+
+    public ListEmptyException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/com/example/phone_duck/handler/ChatRoomRestExceptionHandler.java
+++ src/main/java/com/example/phone_duck/handler/ChatRoomRestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.phone_duck.handler;
 
 import com.example.phone_duck.Model.ErrorResponse;
+import com.example.phone_duck.exception.ListEmptyException;
 import com.example.phone_duck.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,11 @@ public class ChatRoomRestExceptionHandler {
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(ListEmptyException e){
+        ErrorResponse error = new ErrorResponse(HttpStatus.NO_CONTENT.value());
+        return new ResponseEntity<>(error, HttpStatus.NO_CONTENT);
     }
 }
